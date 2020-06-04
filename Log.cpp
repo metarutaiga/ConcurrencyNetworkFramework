1be107fcfeb690f1e2ad775a31307de882f97507
TAiGA <>
2020-06-05 00:29:19 +0800

    Improvement Log

--- Log.cpp
+++ Log.cpp
@@ -6,26 +6,57 @@
 //==============================================================================
 #include <stdio.h>
 #include <stdarg.h>
+#include <alloca.h>
 #include "Log.h"
 
 //------------------------------------------------------------------------------
-void Log::Format(int level, const char* format, ...)
+void (*Log::Output)(const char* format) = Log::DefaultOutput;
+void (*Log::Format)(int level, const char* format, ...) = Log::DefaultFormat;
+//------------------------------------------------------------------------------
+void Log::SetOutput(void(*output)(const char*))
 {
+    Log::Output = output;
+}
+//------------------------------------------------------------------------------
+void Log::SetFormat(void(*format)(int, const char*, ...))
+{
+    Log::Format = format;
+}
+//------------------------------------------------------------------------------
+void Log::DefaultOutput(const char* output)
+{
+    printf("%s", output);
+}
+//------------------------------------------------------------------------------
+void Log::DefaultFormat(int level, const char* format, ...)
+{
+    char replaceFormat[256];
     switch (level)
     {
     case -1:
-        printf("[%s] ", "ERROR");
+        snprintf(replaceFormat, 256, "[%s] %s\n", "ERROR", format);
         break;
 
     default:
-        printf("[%s] ", "INFO");
+        snprintf(replaceFormat, 256, "[%s] %s\n", "INFO", format);
         break;
     }
 
     va_list va;
     va_start(va, format);
-    vprintf(format, va);
-    printf("%s", "\n");
+
+    va_list vaCount;
+    va_copy(vaCount, va);
+    size_t count = vsnprintf(nullptr, 0, replaceFormat, vaCount);
+    va_end(vaCount);
+
+    va_list vaOutput;
+    va_copy(vaOutput, va);
+    char* buffer = (char*)alloca(count + 1);
+    vsnprintf(buffer, count + 1, replaceFormat, vaOutput);
+    Log::Output(buffer);
+    va_end(vaOutput);
+
     va_end(va);
 }
 //------------------------------------------------------------------------------
--- Log.h
+++ Log.h
@@ -9,5 +9,12 @@
 class Log
 {
 public:
-    static void Format(int level, const char* format, ...);
+    static void (*Output)(const char* format);
+    static void (*Format)(int level, const char* format, ...);
+public:
+    static void SetOutput(void(*output)(const char*));
+    static void SetFormat(void(*format)(int, const char*, ...));
+private:
+    static void DefaultOutput(const char*);
+    static void DefaultFormat(int level, const char* format, ...);
 };
